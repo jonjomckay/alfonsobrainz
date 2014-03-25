9777d23a70e037013baff0ef51422efb0b04a41f
Jonjo McKay <jonjo@jonjomckay.com>
2014-03-25 22:56:47 +0000

    Added get_recording_by_id method

--- alfonsobrainz/musicbrainz.py
+++ alfonsobrainz/musicbrainz.py
@@ -1,1 +1,32 @@
-__author__ = 'jonjo'
+import requests
+
+VERSION = '0.5'
+
+def get_version():
+    return VERSION
+
+def get_recording_by_id(mbid, includes=[]):
+    return _send_query('recording', mbid, includes)
+
+def _generate_url(path):
+    return 'https://musicbrainz.org/ws/2/%s' % path
+
+def _send_query(entity, mbid, includes=[]):
+    if not isinstance(includes, list):
+        includes = [includes]
+
+    # TODO: Check to see if includes are valid
+
+    args = {}
+
+    if includes.__len__() > 0:
+        args['inc'] = ' '.join(includes)
+
+    path = '%s/%s' % (entity, mbid)
+
+    return _send_get_request(path, args)
+
+def _send_get_request(path, params):
+    params['fmt'] = 'json'
+
+    return requests.get(_generate_url(path), params=params).json()
